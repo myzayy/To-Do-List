67c99ba09e34c15d3ef40bb9206d946b0dc8f842
myzayy <zaetsmaks06@gmail.com>
2025-05-07 17:27:38 +0200

    Сreate connect.php

---
+++ config/connect.php
@@ -0,0 +1,20 @@
+<?php
+// data for connect
+$host = 'localhost';      // server
+$dbname = 'todo_list';  // name
+$username = 'root';       // username
+$password = '';           // password
+
+// connect by mysqli
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// 3. connection check
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connection succes!";
+}
+
+// connection close
+$conn->close();
+?>
